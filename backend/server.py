a751c3b2ce423592f562a9412c257db264328130
emosahin31-web <emosahin31@gmail.com>
2026-05-09 16:36:50 +0300

    comma fix

--- backend/server.py
+++ backend/server.py
@@ -85,7 +85,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
